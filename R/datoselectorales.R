e342321ae78bd98332662505b3f6c38cd97d886d
Soledad Araya <snaraya@uc.cl>
2020-10-12 21:30:04 -0300

    base de alcaldes

--- R/datoselectorales.R
+++ R/datoselectorales.R
@@ -1,28 +1,69 @@
-#' @title datoselectorales
-#' @description Datos electorales de las elecciones presidenciales en Chile desde 1989.
-#' @format Contiene 64366 filas y 21 columnas.
+#' Paquete con datos electorales de Chile
+#'
+#' Este paquete contiene información eletoral de Chile,
+#' sólo se han fomateado los nombres y algunos partidos
+#' políticos con nombres en distintos formatos.
+#' @docType package
+#' @name datoselectorales
+NULL
+
+#' "Elecciones presidenciales en Chile: 1989-2017"
+#'
+#' @format Una base de datos con 64366 filas y 21 columnas:
 #' \describe{
 #'   \item{\code{tipo_de_eleccion}}{character Tipo de elección. En este caso, presidencial.}
+#'   \item{\code{cargo}}{character Cargo al que se postula el candidato/a.}
 #'   \item{\code{fecha_de_eleccion}}{double Fecha exacta en que se llevó a cabo la elección.}
 #'   \item{\code{ano_de_eleccion}}{double Año en que se llevó a cabo la elección.}
 #'   \item{\code{periodo}}{character Periodo en que gobernará el presidente electo.}
 #'   \item{\code{votacion_presidencial}}{character Si los votos corresponden a primera vuelta, segunda vuelta o elección única (solo en las elecciones de 1989 y 1993).}
 #'   \item{\code{id_region}}{double Número idenficiador de la región.}
 #'   \item{\code{region}}{character Nombre de la región.}
-#'   \item{\code{provincia}}{character Número identificador de la provincia.}
-#'   \item{\code{nombre_provincia}}{character Nombre de la provincia.}
+#'   \item{\code{id_provincia}}{double Número idenficiador de la provincia.}
+#'   \item{\code{provincia}}{character Nombre de la provincia.}
 #'   \item{\code{comuna}}{character Nombre de la comuna.}
-#'   \item{\code{sexo_mesa}}{character Sexo de la mesa.}
-#'   \item{\code{candidato}}{character Nombre completo del candidato.}
-#'   \item{\code{nombres}}{character Nombre del candidato.}
-#'   \item{\code{apellido_paterno}}{character Apellido paterno del candidato.}
-#'   \item{\code{apellido_materno}}{character Apellido materno del candidato.}
-#'   \item{\code{sexo}}{character Sexo del candidato.}
-#'   \item{\code{electo}}{character Si fue electo o no, o si pasó a segunda vuelta.}
-#'   \item{\code{partido}}{character Nombre del partido al que pertenece el candidato.}
-#'   \item{\code{sigla_partido}}{character Sigla del partido al que pertenece el candidato.}
-#'   \item{\code{votos_totales}}{double Votos totales.}
-#'   \item{\code{cargo}}{character Cargo al que se postula el candidato.}
+#'   \item{\code{sexo_mesa}}{character Sexo de la mesa de votación (hasta las elecciones del 2009).}
+#'   \item{\code{candidato}}{character Nombre completo del candidato o candidata.}
+#'   \item{\code{nombres}}{character Nombres del candidato /a.}
+#'   \item{\code{apellido_paterno}}{character Apellido paterno del candidato/a.}
+#'   \item{\code{apellido_materno}}{character Apellido materno del candidato/a.}
+#'   \item{\code{sexo}}{character Sexo al que pertenece el candidato o candidata.}
+#'   \item{\code{electo}}{character Si el candidato o candidata es electo/a o no en las elecciones que compite, o si pasa a segunda vuelta.}
+#'   \item{\code{partido}}{character Partido al que pertenece el candidato/a.}
+#'   \item{\code{sigla_partido}}{character Sigla del partido al que pertenece el candidato/a.}
+#'   \item{\code{votos_totales}}{double Votos totales según corresponda.}
 #'}
 #' @source \url{https://www.servel.cl/resultados-en-excel/}
-"datoselectorales"
+"presidenciales"
+
+#' "Elecciones municipales en Chile: Alcaldes 2004-2016"
+#'@format Base de datos con 18706 filas y 25 columnas:
+#' \describe{
+#'   \item{\code{tipo_de_eleccion}}{character Tipo de elección. En este caso, de alcaldes.}
+#'   \item{\code{cargo}}{character Cargo al que se postula el candidato/a.}
+#'   \item{\code{fecha_de_eleccion}}{double Fecha exacta en que se llevó a cabo la elección.}
+#'   \item{\code{ano_de_eleccion}}{double Año en que se llevó a cabo la elección.}
+#'   \item{\code{inicio_de_periodo}}{double Inicio del periodo del alcalde o alcaldesa electo.}
+#'   \item{\code{fin_de_periodo}}{double Término del periodo del alcalde o alcaldesa electo.}
+#'   \item{\code{periodo}}{character Periodo en que gobernará el alcalde o alcaldesa electo.}
+#'   \item{\code{id_region}}{double Número identificador de la región.}
+#'   \item{\code{region}}{character Nombre de la región.}
+#'   \item{\code{circunscripcion_senatorial}}{double Circunscripción senatorial.}
+#'   \item{\code{distrito}}{double Distrito electoral.}
+#'   \item{\code{comuna}}{character Nombre de la comuna.}
+#'   \item{\code{circunscripcion_electoral}}{double Circunscripción electoral.}
+#'   \item{\code{nombre_circunscripcion_electoral}}{character Nombre de la circunscripción electoral.}
+#'   \item{\code{sexo_mesa}}{character Sexo de la mesa de votación (hasta las elecciones del 2009).}
+#'   \item{\code{candidato}}{character Nombre completo del candidato/a.}
+#'   \item{\code{nombres}}{character Nombre del candidato/a.}
+#'   \item{\code{apellido_paterno}}{character Apellido paterno del candidato/a.}
+#'   \item{\code{apellido_materno}}{character Apellido materno del candidato/a.}
+#'   \item{\code{sexo}}{character Sexo al que pertenece el candidato/a.}
+#'   \item{\code{electo}}{character Si el candidato o candidata es electo/a o no en las elecciones que compite, o si pasa a segunda vuelta.}
+#'   \item{\code{partido}}{character Partido al que pertenece el candidato/a.}
+#'   \item{\code{sigla_partido}}{character Sigla del partido al que pertenece el candidato/a.}
+#'   \item{\code{lista}}{character Lista en la que va el partido del candidato/a.}
+#'   \item{\code{votos_totales}}{double Votos totales según corresponda.}
+#'}
+#' @source \url{https://www.servel.cl/resultados-en-excel/}
+"alcaldes"
--- data-raw/datoselectorales.R
+++ data-raw/datoselectorales.R
@@ -6,31 +6,55 @@ library(janitor)
 library(stringi)
 library(stringr)
 
-prepare_data <- read_excel("data-raw/resultados_elecciones_presidenciales_ce_1989_2017_Chile.xlsx")
+#### PRESIDENCIALES ----
 
-names(prepare_data)
+presidenciales <- read_excel("data-raw/resultados_elecciones_presidenciales_ce_1989_2017_Chile.xlsx")
 
-prepare_data <- prepare_data %>%
+names(presidenciales)
+
+presidenciales <- presidenciales %>%
   clean_names() %>%
   select(tipo_de_eleccion, cargo, fecha_de_eleccion, ano_de_eleccion, periodo, votacion_presidencial,
          id_region, region, provincia, nombre_provincia, comuna, sexo_mesa, candidato_a, nombres, apellido_paterno,
          apellido_materno, sexo, electo_a, partido, sigla_partido, votos_totales) %>%
-  rename(candidato = candidato_a, electo = electo_a)
+  rename(candidato = candidato_a, electo = electo_a, id_provincia = provincia, provincia = nombre_provincia)
 
-prepare_data <- prepare_data %>%
+presidenciales <- presidenciales %>%
   mutate(sigla_partido = gsub('PDC', 'DC', sigla_partido),
          sigla_partido = gsub('INDEP', 'IND', sigla_partido),
          sigla_partido = gsub('PCCH', 'PC', sigla_partido))
 
-prepare_data %>%
+presidenciales %>%
   count(sigla_partido)
 
-prepare_data <- prepare_data %>%
-  mutate_at(vars(tipo_de_eleccion, cargo, votacion_presidencial, region, provincia, comuna, sexo_mesa, candidato, nombres, apellido_materno, apellido_paterno, sexo_mesa, sexo, electo, partido, sigla_partido), ~stri_trans_general(., "latin-ascii")) %>%
-  mutate_at(vars(tipo_de_eleccion, cargo, votacion_presidencial, region, provincia, comuna, sexo_mesa, candidato, nombres, apellido_materno, apellido_paterno, sexo_mesa, sexo, electo, partido, sigla_partido), ~str_to_lower(.))
+glimpse(presidenciales)
 
-prepare_data <- prepare_data %>% select(!cargo, !periodo)
+presidenciales <- presidenciales %>%
+  mutate_if(is.character, ~stri_trans_general(., "latin-ascii")) %>%
+  mutate_if(is.character, ~str_to_lower(.))
 
-datoselectorales <- prepare_data
+head(presidenciales)
 
-usethis::use_data(datoselectorales, overwrite = TRUE)
+usethis::use_data(presidenciales, overwrite = TRUE)
+
+#### MUNICIPALES ----
+
+alcaldes <- read_excel("data-raw/resultados_elecciones_alcaldes_ce_2004_2016.xlsx")
+
+names(alcaldes)
+
+alcaldes <- alcaldes %>%
+  clean_names() %>%
+  rename(candidato = candidato_a, electo = electo_a)
+
+table(alcaldes$sigla_partido)
+
+alcaldes <- alcaldes %>%
+  mutate_if(is.character, ~stri_trans_general(., "latin-ascii")) %>%
+  mutate_if(is.character, ~str_to_lower(.))
+
+head(alcaldes)
+
+usethis::use_data(alcaldes, overwrite = TRUE)
+
+#### CONCEJALES 1992 - 2000
